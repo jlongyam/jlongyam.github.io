9176303c0aee055c21c75b3307db5a6079018516
jlongyam <jhonliongbox@gmail.com>
2023-10-18 03:51:28 +0700

    Add git LFS

--- editor/editor.js
+++ editor/editor.js
@@ -1,6 +1,24 @@
 const editor = {}
 editor.initialize = ()=> {
-  let myEditor = CodeMirror(root, {
+  let layout = new dhx.Layout('root', {
+    type: 'line',
+      rows: [
+        { id: 'header', height: '48px', html: 'Header' },
+        {
+          cols: [
+            { id: 'sideL', width: '200px', collapsable: true, resizable: true, html: 'SideL' },
+            { id: 'main', resizable: true },
+            { id: 'sideR', width: '200px', collapsable: true, html: 'SideR' }
+          ],
+          resizable: true
+        },
+        { id: 'footer', header: 'Footer', height: '120px', collapsable: true, html: 'CONTENT' }
+      ]
+    })
+    layout.getCell('footer').collapse()
+    layout.getCell('sideL').collapse()
+    layout.getCell('sideR').collapse()
+  let editor = CodeMirror( main, {
     value: example.css,
     mode:  "css",
     theme: "pastel-on-dark",
@@ -12,7 +30,7 @@ editor.initialize = ()=> {
       widget: "..."
     },
     keyMap: "sublime"
-  });
+  })
 }
 window.addEventListener('DOMContentLoaded', (e)=> {
   editor.initialize()
